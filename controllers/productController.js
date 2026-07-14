ff5001fabdd921d6fc6db671b344d91375372121
Atul Patel <atulpatel.dev@gmail.com>
2026-07-14 19:44:25 +0530

    Add search and Pegination in Product API

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,7 +3,23 @@ const asyncHandler = require("../utils/asyncHandler");
 const AppError = require("../utils/AppError");
 
 exports.getProducts = asyncHandler(async (req, res) => {
-    const product = await Product.find();
+
+    let query = {};
+
+    if (req.query.search) {
+        query.title = {
+            $regex: req.query.search,
+            $options: "i"
+        };
+    }
+   const page = Number(req.query.page) || 1;
+const limit = Number(req.query.limit) || 5;
+
+const skip = (page - 1) * limit;
+
+    const product = await Product.find(query)
+    .skip(skip)
+    .limit(limit)
     return res.status(200).json(product);
 });
 
